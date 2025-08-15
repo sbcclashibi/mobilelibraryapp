306a3ae86d3fd1d8a38788784900520c7b5f17ba
Jesse Johnson <v-jjohnson@swiftly.com>
2025-08-15 18:10:27 +0000

    Expand version catalog

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.agp)
-    alias(libs.plugins.google.services)
-    alias(libs.plugins.compose)
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.agp) apply false
+    alias(libs.plugins.google.services) apply false
+    alias(libs.plugins.compose) apply false
 }
